25853d705c9631e8f9e54121dc6a818503dada37
IgorL9320 <145779785+IgorL9320@users.noreply.github.com>
2023-10-06 15:59:09 +0200

    Create suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int g, i=0, k;
+    while(scanf("%d", &g) != EOF)
+    {
+        i+=g;
+        printf("%d\n",i);
+    }
+    return 0;
+}
